297d0231d95b195718f5bc7838fd98bd8bc053f1
Austurist <austurist@gmail.com>
2023-06-02 13:55:45 +0200

    Added security headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,12 +12,14 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
+from service import talisman
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
 
 BASE_URL = "/accounts"
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 
 ######################################################################
@@ -34,6 +36,7 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
@@ -197,6 +200,30 @@ class TestAccountService(TestCase):
         # account is really not there
         get_response = self.client.get(f"{BASE_URL}/{account.id}")
         self.assertEqual(get_response.status_code, status.HTTP_404_NOT_FOUND)
+
+    ######################################################################
+    #  S E C U R I T Y   T E S T   C A S E S
+    ######################################################################
+
+    def test_security_headers(self):
+        """
+        Test the existence of security headers
+        """
+
+        expected_headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-XSS-Protection': '1; mode=block',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+
+        resp = self.client.get(f"{BASE_URL}", environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        for (k, v) in expected_headers.items():
+            self.assertIn(k, resp.headers)
+            self.assertEqual(v, resp.headers.get(k))
 
     ######################################################################
     #  S U P P L E M E M N T A R Y   T E S T   C A S E S
